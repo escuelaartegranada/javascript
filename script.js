1497a6f54ca02288b38570115013cc526bbc8e50
sirin <sirin@gmail.com>
2022-09-06 00:37:06 +0200

    Agregando todo

---
+++ script.js
@@ -0,0 +1,6 @@
+const miBoton = document.querySelector("#miBoton");
+const parrafo = document.querySelector("#parrafito");
+miBoton.addEventListener("click", insertarTexto);
+function insertarTexto() {
+    parrafo.innerHTML = "Hola hola";
+}
