fdf0b207b1f5d6ed44690c48c497b5fe5e11604d
sirin <sirin@gmail.com>
2022-09-06 00:43:33 +0200

    Modificando javascript

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const miBoton = document.querySelector("#miBoton");
 const parrafo = document.querySelector("#parrafito");
 miBoton.addEventListener("click", insertarTexto);
 function insertarTexto() {
-    parrafo.innerHTML = "Hola hola";
+    parrafo.innerHTML = "Hola hola hola";
 }
